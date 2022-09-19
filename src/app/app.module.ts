a48cb30dac4c181a676af5af76920e7fdff0c489
renzo gasparini <renzogasparini1@hotmail.com>
2022-09-18 22:59:27 -0300

    entrega 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { ContentComponent } from './component/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
