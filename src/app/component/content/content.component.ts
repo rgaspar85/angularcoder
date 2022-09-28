8df3664bef97de1aa5841fd3884ac65866d5bf49
renzo gasparini <renzogasparini1@hotmail.com>
2022-09-27 22:55:31 -0300

    Interpolación y Directivas

--- src/app/component/content/content.component.ts
+++ src/app/component/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Alumnos } from 'src/app/models/alumnos';
 
 @Component({
   selector: 'app-content',
@@ -6,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+
+  public ALUMNOS : Array<Alumnos> = [
+    {
+      nombre : "juan",
+      edad: 20,
+      ficha: "../../../assets/images/juan.jpg",
+      curso: "Curso Piloto",
+      fecha: new Date(),
+    },
+    {
+      nombre : "Pedro",
+      edad: 26,
+      ficha: "../../../assets/images/pedro.jpg",
+      curso: "Curso Programacion",
+      fecha: new Date(),
+    },
+    {
+      nombre : "Galo",
+      edad: 29,
+      ficha: "../../../assets/images/galo.jpg",
+      curso: "Curso Piloto",
+      fecha: new Date(),
+    },
+    {
+      nombre : "Celeste",
+      edad: 21,
+      ficha: "../../../assets/images/celes.jpg",
+      curso: "Curso Seguridad",
+      fecha: new Date(),
+    },
+    {
+      nombre : "Charo",
+      edad: 33,
+      ficha: "../../../assets/images/charo.jpg",
+      curso: "Curso Programacion",
+      fecha: new Date()
+    }
+  ]
 
   constructor() { }
 
---
+++ src/app/models/alumnos.ts
@@ -0,0 +1,7 @@
+export interface Alumnos{
+    nombre : string;
+    edad: number;
+    ficha: string;
+    curso: string;
+    fecha: Date;
+}
